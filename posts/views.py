6072f0e0b41ca2d02c50fa9b2318bf3b7e3e8f77
hanseokhee <hanseokhee.han@gmail.com>
2022-12-07 21:15:00 +0900

    before pagenation

--- posts/views.py
+++ posts/views.py
@@ -5,29 +5,31 @@ from .forms import PostForm
 
 # Create your views here.
 def index(request):
-  return redirect('post-list')
+    return redirect('post-list')
 
 
 def post_list(request):
     posts = Post.objects.all()
-    context = {"posts":posts}
-    return render(request, 'posts/post_list.html',context)
+    context = {'posts': posts}
+    return render(request, 'posts/post_list.html', context)
+
 
 def post_detail(request, post_id):
-  post = get_object_or_404(Post, id=post_id)
-  context = {"post": post}
-  return render(request, 'posts/post_detail.html',context)
+    post = get_object_or_404(Post, id=post_id)
+    context = {'post': post}
+    return render(request, 'posts/post_detail.html', context)
+
 
 def post_create(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         post_form = PostForm(request.POST)
         if post_form.is_valid():
             new_post = post_form.save()
-        return redirect('post-detail', post_id=new_post.id)
-        
+            return redirect('post-detail', post_id=new_post.id)
     else:
         post_form = PostForm()
-    return render(request, 'posts/post_form.html', {'form':post_form} )
+    return render(request, 'posts/post_form.html', {'form': post_form})
+
 
 def post_update(request, post_id):
     post = get_object_or_404(Post, id=post_id)
@@ -35,15 +37,16 @@ def post_update(request, post_id):
         post_form = PostForm(request.POST, instance=post)
         if post_form.is_valid():
             post_form.save()
-            return redirect('post-detail', post_id=post.id)
+            return redirect('post-detail', post_id=post.id) 
     else:
         post_form = PostForm(instance=post)
-    return render(request, 'posts/post_form.html', {'form':post_form})
+        return render(request, 'posts/post_form.html', {'form': post_form})
+
 
 def post_delete(request, post_id):
-  post = get_object_or_404(Post, id=post_id)
-  if request.method == 'POST':
-    post.delete()
-    return redirect('post-list')
-  else:
-    return render(request, 'posts/post_confirm_delete.html',{'post': post})
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post-list')
+    else:
+        return render(request, 'posts/post_confirm_delete.html', {'post': post})
