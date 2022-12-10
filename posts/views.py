02c104ab60d679a0e35549fdf3857e3f539ba72f
hanseokhee <hanseokhee.han@gmail.com>
2022-12-10 19:20:51 +0900

    listview study

--- posts/views.py
+++ posts/views.py
@@ -1,23 +1,35 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator
+from django.views import View
+from django.views.generic import CreateView, ListView
+from django.urls import reverse
 from .models import Post
 from .forms import PostForm
-from django.core.paginator import Paginator
 
 # Create your views here.
+
 def index(request):
     return redirect('post-list')
 
 
-def post_list(request):
-    posts = Post.objects.all()
-    paginator = Paginator(posts, 6)
-    curr_page_number = request.GET.get('page')
-    if curr_page_number is None:
-      curr_page_number = 1
-    page = paginator.page(curr_page_number)
-    return render(request,'posts/post_list.html', {'page':page})
-    context = {'posts': posts}
-    return render(request, 'posts/post_list.html', context)
+# def post_list(request):
+#     posts = Post.objects.all()
+#     paginator = Paginator(posts, 6)
+#     curr_page_number = request.GET.get('page')
+#     if curr_page_number is None:
+#       curr_page_number = 1
+#     page = paginator.page(curr_page_number)
+#     return render(request,'posts/post_list.html', {'page':page})
+
+
+class PostListView(ListView):
+  model = Post
+  template_name = 'posts/post_list.html'
+  context_object_name = 'posts'
+  ordering = ['-dt_created']
+  paginate_by = 6
+  page_kwarg = 'page'
+
 
 
 def post_detail(request, post_id):
@@ -26,16 +38,34 @@ def post_detail(request, post_id):
     return render(request, 'posts/post_detail.html', context)
 
 
-def post_create(request):
-    if request.method == 'POST':
-        post_form = PostForm(request.POST)
-        if post_form.is_valid():
-            new_post = post_form.save()
-            return redirect('post-detail', post_id=new_post.id)
-    else:
-        post_form = PostForm()
-    return render(request, 'posts/post_form.html', {'form': post_form})
+# def post_create(request):
+#     if request.method == 'POST':
+#         post_form = PostForm(request.POST)
+#         if post_form.is_valid():
+#             new_post = post_form.save()
+#             return redirect('post-detail', post_id=new_post.id)
+#     else:
+#         post_form = PostForm()
+#     return render(request, 'posts/post_form.html', {'form': post_form})
 
+# class PostCreateView(View):
+#   def get(self, request):
+#     post_form = PostForm()
+#     return render(request, 'posts/post_form.html', {'form': post_form})
+
+#   def post(self, request):
+#     post_form = PostForm(request.POST)
+#     if post_form.is_valid():
+#       new_post = post_form.save()
+#       return redirect('post-detail', post_id=new_post.id)
+
+class PostCreateView(CreateView):
+  model = Post
+  form_class = PostForm
+  template_name = 'posts/post_form.html'
+  
+  def get_success_url(self):
+    return reverse('post-detail', kwargs={'post_id': self.object.id})
 
 def post_update(request, post_id):
     post = get_object_or_404(Post, id=post_id)
